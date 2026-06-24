53e458e1ea6f01aecbfa6a7ec1df7b959ec11268
JosephChirra <josephchirra43@gmail.com>
2026-06-24 11:25:16 +0530

    database 2

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 DATABASE_URL = (
-    "postgresql://joseph-postgres-db:Danielchirra343@joseph-postgres-db.czu2scqkekna.ap-south-2.rds.amazonaws.com:5432/fastapi_db"
+    "postgresql://postgres:Danielchirra343@joseph-postgres-db.czu2scqkekna.ap-south-2.rds.amazonaws.com:5432/fastapi_db"
 )
 
 engine = create_engine(DATABASE_URL)
